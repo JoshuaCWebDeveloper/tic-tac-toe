c677e9597e25209ea919e264689c78f9fee6b0f9
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2015-08-07 14:55:36 -0700

    Use perfnow to exted window.performance support to older browsers in benchmark.js

--- js/benchmark.js
+++ js/benchmark.js
@@ -2,9 +2,32 @@
  * Simple ultra-lightweight tool that calculates elapsed time between checkpoints.
  * Author: Joshua Carter
  * Created: May 19, 2014
- * Version: 0.1.1
+ * Version: 0.1.2
  */
 (function () {
+    
+    /* Use perfnow to support older browsers, BEGIN perfnow *****/
+    /**
+     * @file perfnow is a 0.14 kb window.performance.now high resolution timer polyfill with Date fallback
+     * @author Daniel Lamb <dlamb.open.source@gmail.com>
+     */
+    // make sure we have an object to work with
+    if (!('performance' in window)) {
+        window.performance = {};
+    }
+    var perf = window.performance;
+    // handle vendor prefixing
+    window.performance.now = perf.now ||
+        perf.mozNow ||
+        perf.msNow ||
+        perf.oNow ||
+        perf.webkitNow ||
+        // fallback to Date
+        Date.now || function () {
+            return new Date().getTime();
+        };
+    /**** END perfnow *****/
+    
     var newTime = window.performance.now();
     window.benchmark = {
         start: newTime,
