c0b802a9a624a801cb289b6097f345c1124b6608
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2015-08-07 09:22:31 -0700

    Create Game directive and controller to output game view

---
+++ angular/components/game/GameDirective.js
@@ -0,0 +1,45 @@
+/* GameDirective.js
+ * Controls and outputs everything for the game
+ * Dependencies: app module, Game service
+ * Author: Joshua Carter
+ * Created: August 07, 2014
+ */
+"use strict";
+//create controller function for game
+var GameController = function(Game) {
+    //create game
+    var game = new Game(); 
+    //init info
+    this.rounds = [];
+    this.score = {};
+    this.numRounds = 0;
+    //method to start game
+    this.start = function () {
+        //setup game
+        game.init();
+        //store game rounds
+        this.rounds = game._rounds;
+        //start game
+        game.start();
+        //get score
+        this.scores = {
+            'X': game.getScore('X'),
+            'O': game.getScore('O')
+        };
+        //count number of rounds
+        this.numRounds = this.rounds.length;
+    };
+};
+//inject dependencies into controller
+GameController.$inject = ["Game"];
+//create directive for round and add to app
+app.directive("gameView", function () {
+    return {
+        restrict: 'E',
+        scope: {},
+        controller: GameController,
+        controllerAs: 'gameInfo',
+        templateUrl: 'angular/components/game/GameTemplate.html'
+    };
+});
+ 
