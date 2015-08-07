fed4a8c48df7258ace5774f3f81ddcca8d46fd2b
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2015-08-06 21:54:58 -0700

    Create Round.takeTurn() method to play one turn in a round

--- angular/shared/Round.js
+++ angular/shared/Round.js
@@ -27,6 +27,33 @@ var RoundService = function (Board) {
         //returns the board object for this round
         board: function () {
             return this._board;    
+        },
+        //takes a turn at play (places either an 'X' or 'O' in an empty box)
+        takeTurn: function (play) {
+            //get empty boxes
+            var emptyBoxes = this.board().getEmptyBoxes(), 
+                boxIndex, boxLines;
+            do {
+                //randomly select empty box to play in
+                boxIndex = Math.floor(Math.random() * emptyBoxes.length);
+                //the box will no longer be empty
+                this.board().fillBox(boxIndex);
+                //in case our box wasn't empty or the play wasn't otherwise successful, 
+                //keep trying until we find an empty one
+            }
+            while (!emptyBoxes[boxIndex].setPlay(play));
+            //get the lines of our chosen box
+            boxLines = emptyBoxes[boxIndex].lines();
+            //loop through the box's lines
+            for (var i=0; i<boxLines.length; i++) {
+                //update the status of the line, if we won
+                if (boxLines[i].updateStatus() == 'win') {
+                    //stop everything, we won!
+                    return true;
+                }
+            }
+            //we didn't win, so return false
+            return false;
         }
     });
     //return constructor
