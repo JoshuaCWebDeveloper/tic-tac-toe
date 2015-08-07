9f6410ede0bc03805a429c6f21730306dfdac57e
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2015-08-06 19:04:29 -0700

    Rewrite RoundController to control Round instead of Board

--- angular/components/round/RoundController.js
+++ angular/components/round/RoundController.js
@@ -1,15 +1,18 @@
 /* RoundController.js 
  * Create controller class for single round of Tic-Tac-Toe.
- * Dependencies: app module, Board service
+ * Dependencies: app module, Round service
  * Author: Joshua Carter
  * Created: August 06, 2015
  */
 "use strict";
 //create round controlller constructor
-var RoundController = function(board) {
+var RoundController = function(Round) {
+
+    //create round
+    this.round = new Round();
     
 };
 //inject dependencies
-RoundController.$inject = ["Board"];
+RoundController.$inject = ["Round"];
 //add controller to our app
 app.controller("RoundController", RoundController);
